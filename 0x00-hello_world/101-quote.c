05dff041c4d10c5812b6c1a16c67e2f258832868
meriemBoubaker56 <meriemboubaker56@gmail.com>
2023-03-16 15:47:47 -0700

    smth

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *smth smth 
-*/
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(STDOUT_FILENO,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
