1f0884a1d6cc6fac1e04ded1d7841b0b3543f625
meriemBoubaker56 <meriemboubaker56@gmail.com>
2023-03-16 15:53:35 -0700

    smth

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
-/** main - Entry point 
- * Description: 'the program's description'
- * Return: Always 0 (Success)
- */
+/**
+* main - Entry point 
+Description: 'the program's description'
+Return: Always 0 (Success)
+*/
 int main(void)
 {
 write(STDOUT_FILENO,
