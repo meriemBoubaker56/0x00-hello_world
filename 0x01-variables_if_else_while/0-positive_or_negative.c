25004cee80aa48ef40dab5afacf56b5f9088378e
meriemBoubaker56 <meriemboubaker56@gmail.com>
2023-03-17 13:30:51 -0700

    smth

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,14 +15,14 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if(n>0)
 {
-printf("is positive");
+printf("%d is positive",n);
 }
 else if(n==0){
-printf("is zero");
+printf("%d is zero",n);
 }
 else
 {
-printf("is negative");
+printf("%d is negative",n);
 }
 return (0);
 }
