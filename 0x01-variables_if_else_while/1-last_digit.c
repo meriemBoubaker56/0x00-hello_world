16fb4df035aafe0ad6460445cc4492632a1c6cfe
meriemBoubaker56 <meriemboubaker56@gmail.com>
2023-03-25 12:36:50 -0700

    8asret lkleb

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,18 +14,18 @@ int n;
 int mod;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-mod = abs(n%10);
+mod = abs(n % 10);
 if (mod > 5)
 {
-printf("Last digit of %d is %d and is greater than 5", n, mod);
+printf("Last digit of %d is %d and is greater than 5\n", n, mod);
 }
 else if (n == 0)
 {
-printf("Last digit of%d is %d and is 0", n, mod);
+printf("Last digit of%d is %d and is 0\n", n, mod);
 }
 else
 {
-printf("Last digit of %d is %d and is less than 6 and not 0", n, mod);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, mod);
 }
 return (0);
 }
