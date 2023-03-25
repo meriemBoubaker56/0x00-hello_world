bed91d74968ab740727cb956f90ec94ba7832249
meriemBoubaker56 <meriemboubaker56@gmail.com>
2023-03-25 12:52:04 -0700

    8asret lkleb

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 
-/**                                                                                                                                                                     
- * main - Entry point                                                                                                                                                   
- *                                                                                                                                                                      
- * Description: 'the program's description'                                                                                                                             
- *                                                                                                                                                                      
- * Return: Always 0 (Success)                                                                                                                                           
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 int i;
-for (i = 65; i < 91; i++)
+for (i = 97; i < 123; i++)
 {
 putchar (i);
 }
-for (i = 97; i < 123; i++)
+for (i = 65; i < 91; i++)
 {
 putchar (i);
 }
