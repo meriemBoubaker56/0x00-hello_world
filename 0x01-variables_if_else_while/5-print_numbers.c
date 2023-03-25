e4191a7717942d65fb2625ab12790881ecde6d4e
meriemBoubaker56 <meriemboubaker56@gmail.com>
2023-03-25 13:02:26 -0700

    8asret lkleb

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+{
+printf(i);
+}
+putchar ('\n');
+return (0);
+}
