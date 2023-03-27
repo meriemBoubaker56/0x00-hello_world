8308bf9c955b72da0f8becb9cbd41bf980cba086
meriemBoubaker56 <meriemboubaker56@gmail.com>
2023-03-27 12:58:49 -0700

    message

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 /**
- * reset_to_98 - change the point
- * @Parameter : *n is pointer of n 
- * Return : n = 98
- */
+* reset_to_98 - change the point
+* @Parameter : *n is pointer of n
+* Return : n = 98
+*/
 void reset_to_98(int *n)
 {
 *n = 98;
